5f4fe01c6be03aedbe1ecaa19cd541cc8a5408e3
kaustubh0402 <bhurekaustubh70@gmail.com>
2020-09-30 04:32:19 +0530

    String Medium

---
+++ LeetCode/String/Medium/Longest commom prefix.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0)
+          return "";   
+        StringBuffer s=new StringBuffer();
+        int min=Integer.MAX_VALUE;
+        String temp="";
+        for(int i=0;i<strs.length;i++)
+        {
+            if(min>strs[i].length())
+            {
+                min=strs[i].length();
+                temp=strs[i];
+            }
+        }
+        for(int i=0;i<min;i++)
+        {
+            int j;
+            for(j=0;j<strs.length;j++)
+            {
+                if(strs[j].charAt(i)!=temp.charAt(i))
+                {
+                    break;
+                }
+            }
+            if(j==strs.length)
+                s.append(temp.charAt(i));
+            else
+                break;
+        }
+        return s.toString();
+    }
+}
