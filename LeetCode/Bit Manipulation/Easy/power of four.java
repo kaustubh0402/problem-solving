de77e7ed2cb1ad8037cbc60f52983010b172b19e
kaustubh0402 <bhurekaustubh70@gmail.com>
2020-10-16 20:53:59 +0530

    Bit Easy'

--- LeetCode/Bit Manipulation/Easy/power of four.java
+++ LeetCode/Bit Manipulation/Easy/power of four.java
@@ -1,6 +1,23 @@
 https://leetcode.com/problems/power-of-four/
 //Problem no : 342
 
+
+//Brute Force
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0)
+            return false;
+        while(n!=1)
+        {
+            if(n%4!=0)
+                return false;
+            n=n/4;
+        }
+        return true;
+    }
+}
+
+
 class Solution {
     public boolean isPowerOfFour(int n) {
         int check=Integer.parseInt("1010101010101010101010101010101",2);
