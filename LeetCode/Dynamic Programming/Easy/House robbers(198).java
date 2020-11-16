f5df83ecd7a995607ff31ea2c8dcbd628ec65dad
kaustubh0402 <bhurekaustubh70@gmail.com>
2020-11-16 23:51:32 +0530

    Dp easy

---
+++ LeetCode/Dynamic Programming/Easy/House robbers(198).java
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/house-robber/
+//problem no : 198
+
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int dp[]=new int[n+1];
+        if(n==0)
+            return dp[0];
+        dp[1]=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            dp[i+1]=Math.max(dp[i],(dp[i-1]+nums[i]));
+        }
+        return dp[n];
+    }
+}
