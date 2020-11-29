e7c1722a9d649473639a7b99f2238da515ce674f
kaustubh0402 <bhurekaustubh70@gmail.com>
2020-11-29 23:33:23 +0530

    Tree Easy

---
+++ LeetCode/Tree/Easy/merge two binary tree (617).java
@@ -0,0 +1,30 @@
+https://leetcode.com/problems/merge-two-binary-trees/
+//Problem no : 617
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1==null)
+            return t2;
+        if(t2==null)
+            return t1;
+        t1.val=t1.val+t2.val;
+        t1.left=mergeTrees(t1.left,t2.left);
+        t1.right=mergeTrees(t1.right,t2.right);
+        return t1;
+    }
+}
