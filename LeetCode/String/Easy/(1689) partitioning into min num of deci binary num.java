74dc476fce38b63058719549be61f4d7ef19cf6c
kaustubh0402 <bhurekaustubh70@gmail.com>
2020-12-16 23:16:53 +0530

    String Easy

---
+++ LeetCode/String/Easy/(1689) partitioning into min num of deci binary num.java
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+//Problem no : 1689
+
+
+class Solution {
+    public int minPartitions(String n) {
+        int max=0;
+        for(int i=0;i<n.length();i++)
+        {
+            if(n.charAt(i)-48>max)
+                max=n.charAt(i)-48;
+        }
+        return max;
+    }
+}
