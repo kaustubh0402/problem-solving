2af09cd3202bdba3fde366577942278a57053dfb
kaustubh0402 <bhurekaustubh70@gmail.com>
2020-12-17 23:19:05 +0530

    codeforces educational round 100

---
+++ Codeforces/Educational round 100/src/educational/round/pkg100/p1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class p1 {
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     if(sc.hasNextInt()) 
+      {
+        int t=sc.nextInt();
+     	while(t-- >0)
+     	{
+           int a=sc.nextInt();
+           int b=sc.nextInt();
+           int c=sc.nextInt();
+           deployp1 obj=new deployp1();
+           boolean ans=obj.solve(a,b,c);
+           if(ans==true)
+               System.out.println("YES");
+           else
+               System.out.println("NO");
+ 	}
+      }
+        
+  }
+    
+}
+class deployp1
+{
+    public boolean solve(int a,int b,int c)
+    {
+        int sum=a+b+c;
+        int small=Math.min(a,Math.min(b,c));
+        if(sum>=7 && sum%9==0 && small>(sum/9))
+            return true;
+        return false;
+    }
+    
+}
---
+++ Codeforces/Educational round 100/src/educational/round/pkg100/p2.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class p2 {
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     if(sc.hasNextInt()) 
+      {
+        int t=sc.nextInt();
+     	while(t-- >0)
+     	{
+           int n=sc.nextInt();
+           int arr[]=new int[n];
+           for(int i=0;i<n;i++)
+           {
+               arr[i]=sc.nextInt();
+           }
+           deploysome obj=new deploysome();
+           obj.solve(n,arr);
+        }
+      }
+  }
+    
+}
+class deploysome
+{
+    public void solve(int n,int a[])
+    {
+        long sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=a[i];
+        }
+        long num=(sum/n);
+        for(int i=0;i<n;i++)
+        {
+            long fake;
+            if(a[i]-(a[i]/num)*num>((a[i]/num)+1)*num-a[i])
+                fake=((a[i]/num)+1)*num;
+            else
+                fake=(a[i]/num)*num;
+            if(fake==0)
+                fake=Math.min(1,num);
+            if(a[i]%num==0 || num%a[i]==0)
+                fake=a[i];
+            System.out.print(fake+" ");
+        }
+        System.out.println();
+    }
+    
+}
