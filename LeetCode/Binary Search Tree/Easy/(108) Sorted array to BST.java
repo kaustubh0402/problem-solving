c34ee99e49ccf0fe1fa66c1ce9649b788baab661
kaustubh0402 <bhurekaustubh70@gmail.com>
2021-01-01 23:39:11 +0530

    BST Easy

---
+++ LeetCode/Binary Search Tree/Easy/(108) Sorted array to BST.java
@@ -0,0 +1,35 @@
+https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+//Problem no : 108
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        int n=nums.length;
+        return helper(nums,0,n-1);
+    }
+    
+    public TreeNode helper(int nums[],int start,int end)
+    {
+        if(start>end)
+            return null;
+        int mid=(start+end)/2;
+        TreeNode root=new TreeNode(nums[mid]);
+        root.left=helper(nums,start,mid-1);
+        root.right=helper(nums,mid+1,end);
+        return root;
+    }
+}
