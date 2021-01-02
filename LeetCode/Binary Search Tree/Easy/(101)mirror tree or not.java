57cf185631f40a1f4617a2f775e5a20e61b04227
kaustubh0402 <bhurekaustubh70@gmail.com>
2021-01-02 13:54:50 +0530

    BST Easy

--- LeetCode/Binary Search Tree/Easy/(101)mirror tree or not.java
+++ LeetCode/Binary Search Tree/Easy/(101)mirror tree or not.java
@@ -32,3 +32,30 @@ class Solution {
         return (r1.val==r2.val && lf && rg);
     }
 }
+
+//iterative solution
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        Queue <TreeNode> q =new LinkedList<>();
+        if(root==null)
+            return true;
+        q.offer(root);
+        q.offer(root);
+        while(!q.isEmpty())
+        {
+            TreeNode t1=q.poll();
+            TreeNode t2=q.poll();
+            if(t1==null && t2==null)
+                continue;
+            if(t1==null || t2==null)
+                return false;
+            if(t1.val!=t2.val)
+                return false;
+            q.offer(t1.left);
+            q.offer(t2.right);
+            q.offer(t1.right);
+            q.offer(t2.left);
+        }
+        return true;
+    }
+}
