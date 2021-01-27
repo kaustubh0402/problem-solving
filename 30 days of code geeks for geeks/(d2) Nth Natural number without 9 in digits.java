27314418a4072d65f09eb95f334120483657441b
kaustubh0402 <bhurekaustubh70@gmail.com>
2021-01-27 12:35:27 +0530

    Day 2

---
+++ 30 days of code geeks for geeks/(d2) Nth Natural number without 9 in digits.java
@@ -0,0 +1,19 @@
+Basically If we remove 9 then our number system will not have any 9
+which further can be concluded that number system with base 9
+Now calculating given decimal number to base 9 number
+
+class Solution {
+    long findNth(long N)
+    {
+        long ans=0;
+        long mul=1;
+        while(N!=0)
+        {
+            long dig=N%9;
+            N=N/9;
+            ans+=(mul*dig);
+            mul=mul*10;
+        }
+        return ans;
+    }
+}
