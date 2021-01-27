3499466a94165774c669062b5c8e469144b66c89
kaustubh0402 <bhurekaustubh70@gmail.com>
2021-01-27 14:11:24 +0530

    Codeforces round 697

--- Codeforces/codeforcesround697/src/codeforcesround697/p3.java
+++ Codeforces/codeforcesround697/src/codeforcesround697/p3.java
@@ -10,14 +10,25 @@ public class p3 {
         int t=sc.nextInt();
      	while(t-- >0)
      	{
-           int n=sc.nextInt();
-           int arr[]=new int[n+1];
-           for(int i=1;i<=n;i++)
+           int a=sc.nextInt();
+           int b=sc.nextInt();
+           int k=sc.nextInt();
+           int boy[]=new int[a+1];
+           int girl[]=new int[b+1];
+           int bp[]=new int[k];
+           int gp[]=new int[k];
+           for(int i=0;i<k;i++)
            {
-               arr[i]=sc.nextInt();
+               bp[i]=sc.nextInt();
+               boy[bp[i]]++;
+           }
+           for(int i=0;i<k;i++)
+           {
+               gp[i]=sc.nextInt();
+               girl[gp[i]]++;
            }
            deploysome obj=new deploysome();
-           int ans=obj.solve(n,arr);
+           long ans=obj.solve(boy,girl,bp,gp,k);
            System.out.println(ans);
         }
       }
@@ -26,23 +37,14 @@ public class p3 {
 }
 class deploysome
 {
-    public int solve(int n,int a[])
+    public long solve(int boy[],int girl[],int bp[],int gp[],int k)
     {
-        int val[]=new int[n+1];
-        val[n]=a[n];
-        for(int i=n-1;i>=1;i--)
+        long ans=0;
+        for(int i=0;i<k;i++)
         {
-            if(i+a[i]>n)
-                val[i]=a[i];
-            else
-                val[i]=val[i+a[i]]+a[i];
+            ans+=(((k-1)-(boy[bp[i]]-1)-(girl[gp[i]]-1)));
         }
-        int ans=0;
-        for(int i=1;i<=n;i++)
-        {
-            ans=Math.max(ans,val[i]);
-        }
-        return ans;
+        return ans/2;
     }
     
 }
