199c3c5bad06349f56288137867c246946e0552d
kaustubh0402 <bhurekaustubh70@gmail.com>
2021-01-28 16:23:53 +0530

    Day 3

--- LeetCode/Greedy/Medium/(gfg)smallest positive integer that can not represented as sum.java
+++ LeetCode/Greedy/Medium/(gfg)smallest positive integer that can not represented as sum.java
@@ -1,3 +1,5 @@
+https://practice.geeksforgeeks.org/problems/b6b608d4eb1c45f2b5cace77c4914f302ff0f80d/1
+
 Given an array of size N, find the smallest positive integer value that cannot be represented as sum of some elements from the array.
 Example 1:
 
