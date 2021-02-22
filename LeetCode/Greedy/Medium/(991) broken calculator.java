a2a50ad049275a4da60b50c1b893abbab43920a0
kaustubh0402 <bhurekaustubh70@gmail.com>
2021-02-22 13:51:30 +0530

    Greedy Medium

--- LeetCode/Greedy/Medium/(991) broken calculator.java
+++ LeetCode/Greedy/Medium/(991) broken calculator.java
@@ -37,3 +37,20 @@ class Solution {
         return ans+X-Y;
     }
 }
+
+
+//Changing x to y
+public int brokenCalc(int X, int Y) {
+        int multiple = 1, res = 0;
+        while (X * multiple < Y) {
+            multiple <<= 1;
+            res++;
+        }
+        int diff = X * multiple - Y;
+        while (diff > 0) {  
+            res += diff / multiple;
+            diff -= diff / multiple * multiple;
+            multiple >>= 1;
+        }
+        return res;
+    }
