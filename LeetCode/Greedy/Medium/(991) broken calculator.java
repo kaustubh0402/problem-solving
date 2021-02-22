fdcca8188d5d22465a4a86f1fb1b26ff08a9952d
kaustubh0402 <bhurekaustubh70@gmail.com>
2021-02-22 13:25:55 +0530

    Greedy Medium

---
+++ LeetCode/Greedy/Medium/(991) broken calculator.java
@@ -0,0 +1,39 @@
+https://leetcode.com/problems/broken-calculator/
+//Problem no : 991
+
+/*
+Explanation:
+Wrong thinking:
+ start from X and we will multiply x by 2 each time till X<y 
+ add X-Y stpes to above steps as answer
+ above method fails
+ consider 3 10
+ By above method:
+ X=6   step=1
+ X=12  step=2;
+ X-Y=12-10=2  total steps=steps+(X-Y)=2+2=4
+ which is wrong 
+ correct answer is  X=6 X=5 and X=10 steps=3
+
+Why above methods fail??
+  Our Aim is to convert X to Y 
+  i.e. Y should final answer
+  so we have to start from end i.e Y  
+*/
+
+class Solution {
+    public int brokenCalc(int X, int Y) {
+        if(X>=Y)
+            return X-Y;
+        int ans=0;
+        while(Y>X)
+        {
+            if((Y&1)==1)
+                Y++;
+            else
+                Y=Y/2;
+            ans++;
+        }
+        return ans+X-Y;
+    }
+}
