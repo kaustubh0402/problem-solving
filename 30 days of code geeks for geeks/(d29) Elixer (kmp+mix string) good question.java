efee39f0208ea1169c8026faff0867eba63deda1
kaustubh0402 <bhurekaustubh70@gmail.com>
2021-02-27 14:20:16 +0530

    Day 29

--- 30 days of code geeks for geeks/(d29) Elixer (kmp+mix string) good question.java
+++ 30 days of code geeks for geeks/(d29) Elixer (kmp+mix string) good question.java
@@ -27,7 +27,89 @@ and suffixes. But, "a" has the maximum
 frequency.
 */
 
-
+/*
+If use of inbuilt function then passing solution
+class Sol
+{
+    int maxFrequency(String s)
+    {
+        int n=s.length();
+    	StringBuilder patt =new StringBuilder();
+    	int i=0;
+    	while(i<n)
+    	{
+    	    patt.append(s.charAt(i));
+    	    if(s.endsWith(patt.toString()))
+    	       break;
+    	    i++;
+    	}
+    	if(patt.length()==n) 
+    	  return 1;
+    	return kmp(s,patt.toString());
+    }
+    
+    int kmp(String s,String patt)
+    {
+        int ans=0;
+        int n=s.length();
+        int m=patt.length();
+        int lps[]=lps(patt);
+        int i=0;
+        int j=0;
+        while(i<n)
+        {
+            if(s.charAt(i)==patt.charAt(j))
+            {
+                j++;
+                i++;
+            }
+            if(j==m)
+            {
+                ans++;
+                j=lps[j-1];
+            }
+            else if(i<n && s.charAt(i)!=patt.charAt(j))
+            {
+                if(j!=0)
+                  j=lps[j-1];
+                else
+                  i++;
+            }
+        }
+        return ans;
+    }
+    
+    int[] lps(String s) {
+        int n=s.length();
+        int lps[]=new int[n];
+        int len=0;
+        lps[0]=0;
+        int i=1;
+        while(i<n)
+        {
+            if(s.charAt(i)==s.charAt(len))
+            {
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else
+            {
+                if(len!=0)
+                {
+                    len=lps[len-1];
+                }
+                else
+                {
+                    lps[i]=len;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
+*/
 class Sol
 {
     int maxFrequency(String s)
