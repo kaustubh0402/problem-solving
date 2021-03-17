9aaf0e80426d034bf31a37e611ceffa33760fd8f
kaustubh0402 <bhurekaustubh70@gmail.com>
2021-03-17 23:56:36 +0530

    codeforces round 708

---
+++ Codeforces/codeforcesround708/problemD.java
@@ -0,0 +1,101 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class problem{
+	static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+	
+	
+    public static void main(String[] args) {
+		 FastReader sc=new FastReader();
+		 int t=sc.nextInt();
+		 while(t-- >0)
+		 {
+			int n=sc.nextInt();
+			int k=sc.nextInt();
+			int c=1;
+			while(k!=3)
+			{
+			    System.out.print(1+" ");
+			    k--;
+			    n--;
+			}
+			int a=n/2;
+			int b=1;
+			if(n%2==1)
+			{
+			    a=(n-1)/2;
+			}
+			else if(a%2==1)
+			{
+			  if(a!=1)
+			  {
+			    a--;
+			    b=2;
+			  }
+			}
+			else
+			{
+			   a=a/2;
+			   b=2*a;
+			}
+			System.out.println(a+" "+a+" "+b);
+		 }
+	}
+}
+  
