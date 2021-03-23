15bea3542b8a6ae5de5d87e8e26fac3b42b9e0d1
kaustubh0402 <bhurekaustubh70@gmail.com>
2021-03-24 01:45:15 +0530

    Dp easy

--- LeetCode/Dynamic Programming/Easy/climbing stairs(70).java
+++ LeetCode/Dynamic Programming/Easy/climbing stairs(70).java
@@ -15,3 +15,25 @@ class Solution {
         return dp[n]=(climbStairs(n-1)+climbStairs(n-2));
     }
 }
+
+
+class Solution {
+    public int climbStairs(int n) {
+        int dp[]=new int[n+1];
+        return helper(0,n,dp);
+    }
+    
+    int helper(int j,int n,int dp[])
+    {
+        if(j==n)
+        {
+            return 1;
+        }
+        if(j>n)
+            return 0;
+        if(dp[j]!=0)
+            return dp[j];
+        dp[j]+=helper(j+1,n,dp)+helper(j+2,n,dp);
+        return dp[j];
+    }
+}
