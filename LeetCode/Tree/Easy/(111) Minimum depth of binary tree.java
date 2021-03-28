6942db99fece035157962b278557afe191f543a7
kaustubh0402 <bhurekaustubh70@gmail.com>
2021-03-28 20:14:51 +0530

    Tree Easy

--- LeetCode/Tree/Easy/(111) Minimum depth of binary tree.java
+++ LeetCode/Tree/Easy/(111) Minimum depth of binary tree.java
@@ -1,6 +1,27 @@
 https://leetcode.com/problems/minimum-depth-of-binary-tree/
 //Problem no : 111
 
+/*
+Explanation:
+Explanation:
+It is same as maximum height in Binary tree
+But we have to take care 1 extra corner case
+Suppose if there is no left root then we can not take left height as 0
+similarly if there is no right root then we can not take right height as 0
+So  we will check if right height or left height is equal to 0 or not
+if it is equal to zero then we will ignore it
+i.e if left height is 0 then we should return right height
+if above condition is not there then it will return 0 which is wrong
+consider following example
+           1
+			  2 
+				  3
+					  4
+						  5
+In above tree min height is 5 not 0
+as there are no left nodes hence 0 should not be height
+*/
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
