c29501444f74a851837f84f4896682023b315916
kaustubh0402 <bhurekaustubh70@gmail.com>
2021-04-05 00:04:36 +0530

    Dp Medium

--- LeetCode/Dynamic Programming/Medium/(673) Number of Longest Increasing Subsequence.java
+++ LeetCode/Dynamic Programming/Medium/(673) Number of Longest Increasing Subsequence.java
@@ -57,3 +57,41 @@ class Solution {
         return ans;
     }
 }
+
+//(n^2) dp solution
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        int n=nums.length;
+        int dp[]=new int[n];
+        int count[]=new int[n];
+        Arrays.fill(dp,1);
+        Arrays.fill(count,1);
+        int mx=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(nums[i]>nums[j])
+                {
+                    if(dp[i]<dp[j]+1)
+                    {
+                        dp[i]=dp[j]+1;
+                        count[i]=count[j];
+                    }
+                    else if(dp[i]==dp[j]+1)
+                    {
+                        count[i]+=count[j];
+                    }
+                    mx=Math.max(mx,dp[i]);
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+            if(dp[i]==mx)
+                ans+=count[i];
+        
+        return ans;
+        
+    }
+}
