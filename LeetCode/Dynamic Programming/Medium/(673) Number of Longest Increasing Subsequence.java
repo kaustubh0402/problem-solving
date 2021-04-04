249744d385d5a7c23c2a6528104b478810c14ef9
kaustubh0402 <bhurekaustubh70@gmail.com>
2021-04-04 23:44:07 +0530

    Dp Medium

---
+++ LeetCode/Dynamic Programming/Medium/(673) Number of Longest Increasing Subsequence.java
@@ -0,0 +1,59 @@
+https://leetcode.com/problems/number-of-longest-increasing-subsequence/
+//Problem no : 673
+
+//Following solution is same as Printing all LIS
+//But it will give MLE error
+
+class Solution {
+    class node
+    {
+        int i;
+        int l;
+        node(int index,int len)
+        {
+            i=index;
+            l=len;
+        }
+    }
+    public int findNumberOfLIS(int[] nums) {
+        int n=nums.length;
+        int dp[]=new int[n];
+        Arrays.fill(dp,1);
+        int mx=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(nums[i]>nums[j])
+                {
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                    mx=Math.max(mx,dp[i]);
+                }
+            }
+        }
+        ArrayDeque<node> q=new ArrayDeque<>();
+        for(int i=0;i<n;i++)
+        {
+            if(dp[i]==mx)
+                q.add(new node(i,mx));
+        }
+        int ans=0;
+        while(!q.isEmpty())
+        {
+            node curr=q.removeFirst();
+            if(curr.l==1)
+            {
+                ans++;
+                continue;
+            }
+            for(int in=0;in<curr.i;in++)
+            {
+                if(dp[in]==curr.l-1 && nums[in]<nums[curr.i])
+                {
+                    q.add(new node(in,curr.l-1));
+                }
+            }
+        }
+        return ans;
+    }
+}
